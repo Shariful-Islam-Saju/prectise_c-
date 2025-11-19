0032e2fb95add6da3fc3e58361b0ae896ea944ae
Shariful Islam <sajukhan12905@gmail.com>
2025-11-19 14:16:16 +0600

    start count

--- project-10.cpp
+++ project-10.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void startPrint(int startCount, int spaceCount) {
+    for (int i = 0; i < startCount; i++) {
+        cout << "*";
+    }
+    cout << endl;
+}
 
-int startPrint(int startCount) {
-  for (int i = 0; i < startCount; i++) {
-    cout << "*";
-  }
-  cout << endl;
-  return 0;
-};
+void spacetPrint(int spaceCount) {
+    for (int i = 0; i < spaceCount; i++) {
+        cout << "*";
+    }
+    cout << endl;
+}
 
-int convertNToLineNumber (int n) {
-  return  ((n+1) / 2 ) + 5 ;
-};
-
-
+int convertNToLineNumber(int n) {
+    return ((n + 1) / 2) + 5;
+}
 
 int main() {
-  int n;
-  cin >> n;
+    int n =3;
 
-  cout << convertNToLineNumber(n) << endl;
-  
-  return 0;
+    int lineNumber = convertNToLineNumber(n);
+
+    for (int i = 1; i <= lineNumber; i++) {
+
+        startPrint(i);
+    }
+
+    return 0;
 }
